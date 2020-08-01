2bd2f549b6f30fae36cb92146c815631ccb2615a
Leizle Ho <ho.leizle@gmail.com>
2020-08-01 00:12:42 -0700

    added function for chain validation

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const cryptoHash = require('./cryptoHash');
 class Blockchain {
   constructor() {
     this.chain = [Block.genesis()];
@@ -12,5 +13,19 @@ class Blockchain {
 
     this.chain.push(newBlock);
   }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      const actualLastHash = chain[i - 1].hash;
+      const { data, hash, lastHash, timestamp } = block;
+      if (lastHash !== actualLastHash) return false;
+
+      const validateHash = cryptoHash(data, lastHash, timestamp);
+      if (hash !== validateHash) return false;
+    }
+    return true;
+  };
 }
 module.exports = Blockchain;
