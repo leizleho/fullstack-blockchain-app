0c567c04f9bc22540e5a0e579bba817ca40ffa08
Leizle Ho <ho.leizle@gmail.com>
2020-08-04 20:30:28 -0700

    prevent redundant interactions -root node publishing toitself

--- index.js
+++ index.js
@@ -44,5 +44,7 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
-  syncChains();
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
 });
